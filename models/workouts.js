14ace912a3ad0a4f26e8666ca0aa2137eb114704
Bobby Jones Jr <jones9682@yahoo.com>
2020-09-19 23:03:07 -0400

    added workouts schema

---
+++ models/workouts.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutsSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: 'Enter type of exercise.'
+        },
+        name: {
+            type: String,
+            required: 'Enter name of exercise.'
+        },
+        duration: {
+            type: Number,
+            required: 'Enter duration in minutes of exercise'
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+});
+
+const workouts = mongoose.model("workouts", workoutsSchema);
+
+module.exports = workouts;
